1d57eb7c3f1b07368f01e9d6537a59f8dd943fd2
Willem van Straten <vanstraten.willem@gmail.com>
2020-10-01 14:43:33 +1300

    coherent debirefraction code integrated

---
+++ Signal/General/PlasmaResponseProduct.C
@@ -0,0 +1,72 @@
+/***************************************************************************
+ *
+ *   Copyright (C) 2020 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+#include "dsp/PlasmaResponseProduct.h"
+
+using namespace std;
+
+dsp::PlasmaResponseProduct::PlasmaResponseProduct ()
+{
+}
+
+//! Destructor
+dsp::PlasmaResponseProduct::~PlasmaResponseProduct ()
+{
+}
+
+//! Create a product to match the input
+void dsp::PlasmaResponseProduct::prepare (const Observation* obs, unsigned nchan)
+{
+  if (verbose)
+    cerr << "dsp::PlasmaResponseProduct::prepare (const Observation*, unsigned nchan)" << endl;
+
+  for (unsigned iresp=0; iresp < response.size(); iresp++)
+  {
+    response[iresp]->prepare (obs, nchan);
+  }
+}
+
+//! Add a response to the product
+void dsp::PlasmaResponseProduct::add_response (PlasmaResponse* _response)
+{
+  response.push_back (_response);
+  _response->changed.connect (this, &PlasmaResponseProduct::set_component_changed);
+}
+
+    //! Called when a component has changed
+void dsp::PlasmaResponseProduct::set_component_changed (const Response& response)
+{
+  set_not_built ();
+}
+
+
+using namespace std;
+
+void dsp::PlasmaResponseProduct::build (unsigned _ndat, unsigned _nchan)
+{
+  if (verbose)
+    cerr << "dsp::PlasmaResponseProduct::build" << endl;
+
+  for (unsigned iresp=0; iresp < response.size(); iresp++)
+  {
+    response[iresp]->build (_ndat, _nchan);
+  }
+  
+  Response::operator = (*response[0]);
+
+  if (verbose)
+    cerr << "dsp::PlasmaResponseProduct::build ndat=" << ndat
+         << " nchan=" << nchan << endl;
+
+  for (unsigned iresp=1; iresp < response.size(); iresp++)
+  {
+    Response::operator *= (*response[iresp]);
+  }
+
+  if (verbose)
+    cerr << "dsp::PlasmaResponseProduct::build done" << endl;
+}
---
+++ Signal/General/dsp/PlasmaResponseProduct.h
@@ -0,0 +1,65 @@
+//-*-C++-*-
+/***************************************************************************
+ *
+ *   Copyright (C) 2020 by Willem van Straten
+ *   Licensed under the Academic Free License version 2.1
+ *
+ ***************************************************************************/
+
+// dspsr/Signal/General/dsp/ResponseProduct.h
+
+#ifndef __ResponseProduct_h
+#define __ResponseProduct_h
+
+#include "dsp/PlasmaResponse.h"
+
+namespace dsp {
+
+  //! Represents a product of PlasmaResponse instances
+  /*! 
+    The dimensions of the product will contain the dimensions of
+    each term in the product, as defined by:
+
+   - the total required frequency resolution per output frequency channel
+   - the largest dimension: matrix > dual polarization > single; complex > real
+
+  */
+  class PlasmaResponseProduct: public PlasmaResponse
+  {
+
+  public:
+
+    //! Default constructor
+    PlasmaResponseProduct ();
+
+    //! Destructor
+    ~PlasmaResponseProduct ();
+
+    //! Prepare a product to match the input
+    void prepare (const Observation* input, unsigned nchan);
+
+    //! Add a response to the product
+    void add_response (PlasmaResponse* response);
+
+    //! Return the effective delay for the given frequency
+    double delay_time (double freq) const;
+    
+  protected:
+
+    //! The responses
+    std::vector< Reference::To<PlasmaResponse> > response;
+
+    //! Flag set true when a component has changed
+    bool component_changed;
+
+    //! Called when a component has changed
+    void set_component_changed (const Response& response);
+
+    //! Construct by combining the reponses of the components
+    void build (unsigned ndat, unsigned nchan);
+    
+  };
+
+}
+
+#endif
